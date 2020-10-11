392b0a92f011bd7972720be47b530ea8e3eddb8c
jardielson <jardielsonbio@gmail.com>
2020-10-11 11:56:04 -0300

    created route of sarch

--- backend/src/controllers/Controllers.js
+++ backend/src/controllers/Controllers.js
@@ -5,13 +5,20 @@ const jwt = require('jsonwebtoken');
 //const { secret } = require('../utils/secret.json');
 const { HTTP_CREATED, HTTP_BAD_REQUEST, HTTP_OK, HTTP_INTERNAL_ERROR, HTTP_NOT_FOUND } = require('../utils/codesHttp');
 
-
+const { Op } = require('sequelize');
 
 function create_token(params) {
     return jwt.sign( { params },process.env.SECRET_ENV,{
         expiresIn:300
     });
  } 
+
+ function authenticate(req,res,next){
+  
+    console.log("middleware");
+
+    next();
+ }
 
 class  controllersRouter{
 
@@ -148,6 +155,28 @@ class  controllersRouter{
         }
     }
 
+   async sarch(req,res){
+      const { name } = req.query;
+      
+    try{
+      const foundName = await User.findAll({where:{
+          [Op.or]:[{ name }]
+      }})
+      .then(function(foundName){
+         if(!foundName)
+            return res.status(HTTP_BAD_REQUEST).json({message:'user not found'});
+        
+         return res.status(HTTP_CREATED).json(foundName);
+      })
+      .catch((err)=>{
+          return res.status(HTTP_BAD_REQUEST).json();
+      })
+    }
+    catch(err){
+        console.log(err);
+        return res.status(HTTP_INTERNAL_ERROR).json({message:"don'n connect with database"});
+    }
+   }
 
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,33 @@
+require('dotenv').config();
 const { Router } = require('express');
 const controller = require('./controllers/Controllers');
+const jwt = require('jsonwebtoken');
 const routes = Router();
 
+function authenticate(req,res,next){
+  var token = req.headers['x-access-token'];
+
+  if(!token)
+    return res.status(500).json({auth: false,message: 'no token provide'});
+ 
+  jwt.verify(token,process.env.SECRET_ENV,function(err,decoded){
+  if(err)
+     return res.status(500).json({auth: false,message: 'failed to authenticate token'});
+   
+  req.userId = decoded.id;
+
+  next();
+    
+    })
+
+ }
 
 routes.post("/create",controller.store);
-routes.get("/loadAll",controller.load);
-routes.get("/loadOne/:id",controller.loadOne);
-routes.post("/update/:id",controller.update);
-routes.get("/destroy/:id",controller.destroy);
+routes.get("/loadAll",authenticate,authenticate,controller.load);
+routes.get("/loadOne/:id",authenticate,controller.loadOne);
+routes.post("/update/:id",authenticate,controller.update);
+routes.get("/destroy/:id",authenticate,controller.destroy);
+routes.get("/sarch",authenticate,controller.sarch);
 
 
 module.exports = routes;
