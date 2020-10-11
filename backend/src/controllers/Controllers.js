d4b12a3436fe0e1ebd923d204aa30f43503713d7
jardielson <jardielsonbio@gmail.com>
2020-10-11 11:14:04 -0300

    create the token

--- backend/src/controllers/Controllers.js
+++ backend/src/controllers/Controllers.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
 const { User } = require('../app/models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+//const { secret } = require('../utils/secret.json');
+const { HTTP_CREATED, HTTP_BAD_REQUEST, HTTP_OK, HTTP_INTERNAL_ERROR, HTTP_NOT_FOUND } = require('../utils/codesHttp');
+
+
+
+function create_token(params) {
+    return jwt.sign( { params },process.env.SECRET_ENV,{
+        expiresIn:300
+    });
+ } 
 
 class  controllersRouter{
 
@@ -20,23 +32,27 @@ class  controllersRouter{
               password:hash
           })
           .then(function(users){
+            const id = users.id;
+            
+            const token =  create_token(id);
 
-            return res.status(200).json(users);
+
+            return res.status(HTTP_CREATED).json({users,token});
           })
           .catch((err)=>{
               console.log(err);
-              res.status(500).json({message:"don't possible registrer user"});
+              return  res.status(HTTP_BAD_REQUEST).json({message:"don't possible registrer user"});
           })
       
           
           }
           catch(err){
               console.log(err);
-              return res.send(err);
+              return res.status(HTTP_BAD_REQUEST).json(err);
           }
         }
         else{
-            return res.status(404).json({message:"email already exist"});
+            return res.status(HTTP_BAD_REQUEST).json({message:"email already exist"});
         }
    
     }
@@ -45,14 +61,14 @@ class  controllersRouter{
         try{
           const data = await User.findAll()
           .then(function(data){
-              return res.status(200).json(data);
+              return res.status(HTTP_OK).json(data);
           })
           .catch((err)=>{
-              return res.status(500).json({message:"don't connect with database"});
+              return res.status(HTTP_INTERNAL_ERROR).json({message:"don't connect with database"});
           })
         }
         catch(err){
-            return res.status(500).json({message:"don't connect with database"})
+            return res.status(HTTP_INTERNAL_ERROR).json({message:"don't connect with database"})
         }
     }
 
@@ -62,15 +78,15 @@ class  controllersRouter{
           const data = await User.findByPk(req.params.id)
           
           if(data){
-              return res.status(200).json(data);
+              return res.status(HTTP_OK).json(data);
           }
           else{
-              return res.status(500).json({message:"user not found"});
+              return res.status(HTTP_NOT_FOUND).json({message:"user not found"});
           }
         
         }
         catch(err){
-            return res.status(500).json({message:"don't connect with database"})
+            return res.status(HTTP_INTERNAL_ERROR).json({message:"don't connect with database"})
         }
     }
 
@@ -89,19 +105,19 @@ class  controllersRouter{
                   password:hash
               })
               .then(function(data){
-                  return res.status(200).json(data);
+                  return res.status(HTTP_OK).json(data);
               })
               .catch((err)=>{
-                  return res.status(400).json({message:"don't possible update datas"});
+                  return res.status(HTTP_BAD_REQUEST).json({message:"don't possible update datas"});
               })
           }
           else{
-              return res.status(500).json({message:"user not found"});
+              return res.status(HTTP_NOT_FOUND).json({message:"user not found"});
           }
         
         }
         catch(err){
-            return res.status(500).json({message:"don't connect with database"})
+            return res.status(HTTP_INTERNAL_ERROR).json({message:"don't connect with database"})
         }
     }
 
@@ -116,19 +132,19 @@ class  controllersRouter{
                   id:data
               }})
               .then(function(data){
-                  return res.status(200).json({message:"user deleted with sucess"});
+                  return res.status(HTTP_OK).json({message:"user deleted with sucess"});
               })
               .catch((err)=>{
-                  return res.status(400).json({message:"don't possible delete datas"});
+                  return res.status(HTTP_BAD_REQUEST).json({message:"don't possible delete datas"});
               })
           }
           else{
-              return res.status(500).json({message:"user not found"});
+              return res.status(HTTP_BAD_REQUEST).json({message:"user not found"});
           }
         
         }
         catch(err){
-            return res.status(500).json({message:"don't connect with database"})
+            return res.status(HTTP_INTERNAL_ERROR).json({message:"don't connect with database"})
         }
     }
 
