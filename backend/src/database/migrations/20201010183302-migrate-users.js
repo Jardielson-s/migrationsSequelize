2b06a11513ce0a1838fef3f9961f245572c31574
jardielson <jardielsonbio@gmail.com>
2020-10-12 13:07:53 -0300

    modify migrate users

--- backend/src/database/migrations/20201010183302-migrate-users.js
+++ backend/src/database/migrations/20201010183302-migrate-users.js
@@ -20,15 +20,6 @@ module.exports = {
       type: Sequelize.STRING,
       allowNull: false,
     },
-    
-    People_id: {
-      type: Sequelize.INTEGER,
-      allowNull:false,
-      references: {
-        model: 'People',
-        key: 'id'
-      }
-    },
     created_at: {
       type: Sequelize.DATE,
       allowNull: false,
